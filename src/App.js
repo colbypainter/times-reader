9c265ea50b998024a9866383e121be4801bbdd6d
colbypainter <colby.painter@gmail.com>
2017-09-07 05:12:14 +0000

    lotsa things + get request works and stores in state

--- src/App.js
+++ src/App.js
@@ -15,16 +15,14 @@ class App extends Component {
     super(props);
     this.state = {
       section: "home",
+      message: "",
       results: []
     };
 
     this.changeSection = this.changeSection.bind(this);
     this.apiRequest = this.apiRequest.bind(this);
     this.updateResults = this.updateResults.bind(this);
-  }
-
-  componentDidMount() {
-    this.apiRequest();
+    this.updateMessage = this.updateMessage.bind(this);
   }
   
   changeSection(event) {
@@ -37,21 +35,38 @@ class App extends Component {
   apiRequest() {
     var myUrl = "https://api.nytimes.com/svc/topstories/v2/" + this.state.section + ".json";
     console.log(myUrl);
-    request.get({
-      url: myUrl,
-      qs: {
-        'api-key': "59044bcf3f134f5995553747094e4035"
-      },
-    }, function(err, response, body) {
-      if (err) {
-        console.log(err);
-        //return;
+    var data;
+    function getRequest(url, callback) {  
+      request.get({
+          url: myUrl,
+          qs: {
+            'api-key': "59044bcf3f134f5995553747094e4035"
+          },
+        }, function(err, response, body) {
+            console.log(err);
+            if(err) {
+              console.log("Error: " + err);
+              callback(err);
+            }
+            // TODO I don't like this dual error handling, short-term fix. Revisit this.
+            try {
+              data = JSON.parse(body);
+              console.log(data);
+              callback(data);
+            } catch(e) {
+              console.log(e.message);
+              callback(e.message);
+            }
+        });
       }
-      console.log(body);
-      body = JSON.parse(body);
-      console.log(body);
-      this.updateResults(body);
-    });
+    getRequest(myUrl, this.updateResults);
+  }
+  
+  updateMessage(msg) {
+    var newMessage = msg;
+    this.setState((state, props) => ({
+      message: newMessage
+    }));
   }
   
   updateResults(data) {
@@ -59,6 +74,7 @@ class App extends Component {
     this.setState((state, props) => ({
       results: newResults
     }));
+    console.log(this.state.results);
   }
   
   render() {
@@ -70,8 +86,29 @@ class App extends Component {
           <p className="App-intro">
             Read the news...for yer health!
           </p>
+          <Results section={this.state.section} 
+                    message={this.state.message}
+                    results={this.state.results}
+                    apiRequest={this.apiRequest}
+                    changeSection={this.changeSection}
+                    updateMessage={this.updateMessage}
+                    updateResults={this.updateResults} />
         </div>
     );
+  }
+  
+}
+
+class Results extends Component {
+  
+  componentDidMount() {
+    this.props.apiRequest();
+  }
+  
+  render() {
+    return(
+        <h3>Test</h3>
+      );
   }
 }
 
